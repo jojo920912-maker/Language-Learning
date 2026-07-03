5b5fc6dd6e2f53c5b561c5afbded15b5b7b86af1
Rzhe <jojo920912@gmail.com>
2026-07-03 22:13:56 +0800

    docx: Add base to github

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Language-Learning/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
